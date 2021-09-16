5745d567e65d5021a42adb165bb3173fe4582724
Thomas Fowler <thomas.fowler@flatironschool.com>
2021-09-16 15:52:47 -0600

    Initial solution.

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,19 +1,25 @@
-function reverseString(str) {
-  // type your code here
+const reverseString = (str) => {
+  // need to iterate from back to front
+  // and populate new array; join once
+  // complete
+  return str.split('').reduce((p, c, index, chars) => {
+    p.push(chars[(chars.length - 1) - index])
+    return p
+  }, []).join('')
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+  console.log("Expecting: 'ih'")
+  console.log("=>", reverseString("hi"))
 
-  console.log("");
+  console.log("")
 
-  console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
+  console.log("Expecting: 'ybabtac'")
+  console.log("=>", reverseString("catbaby"))
 }
 
-module.exports = reverseString;
+module.exports = reverseString
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
